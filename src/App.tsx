081da3aa570266e1f5759de74dd16eab0ad1a043
Filip <f.jaros05@seznam.cz>
2025-06-27 14:22:00 +0200

    create components folder and login file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Login from './components/login'
+
 
 function App() {
   return (
-    <></>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
