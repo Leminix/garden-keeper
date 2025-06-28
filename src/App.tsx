bfbbcce7dc2d2d4e0bff5556316336d90357d700
Filip <f.jaros05@seznam.cz>
2025-06-28 14:41:39 +0200

    create frontend for login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './components/login'
 
-
 function App() {
   return (
     
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,8 +1,58 @@
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
 
 const Login = () => {
+  useEffect(() => {
+    const user_name = document.getElementById('user_name') as HTMLInputElement
+    const user_password = document.getElementById('user_password') as HTMLInputElement
+    const btn = document.getElementById('login_button') as HTMLButtonElement
+
+    btn.addEventListener('click', (e) => {
+
+      e.preventDefault()
+      if(user_name.value === '' || user_password.value === '') {    // check if fields are empty
+        alert('Please fill in all fields')
+        return
+      }
+      else{
+
+        const userlogin = {     // create userlogin object
+          user_name: user_name.value,
+          user_password: user_password.value
+        }
+
+        const wss = new WebSocket('ws://localhost:8080')  // create websocket connection
+
+        wss.onopen = () => {
+          wss.send(JSON.stringify(userlogin))
+          wss.onmessage = (e) => {
+            const response = JSON.parse(e.data)  // parse the response from the server
+            if(response.status === 'true'){
+
+              <Link to="/dashboard"></Link>  // redirect to dashboard if login is successful
+
+            }
+            
+          }
+        }
+
+      }
+
+    })
+
+
+
+  }, [])
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div id="login_container" className="login_container">
+      
+      <form id="login_form" className="login_form">
+        <input type="text" id="user_name" className="login_input" placeholder="Enter user name" required/>
+        <input type="password" id="user_password" className="login_input" placeholder="Enter user password" required/>
+        <button type="submit" id="login_button" className="login_button">Login</button>
+      </form>
+
     </div>
   )
 }
